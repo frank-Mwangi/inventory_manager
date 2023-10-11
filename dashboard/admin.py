bff5bc8d197b8f3baee5c650ba8b5033437a417c
frank-Mwangi <kungofrank@gmail.com>
2023-10-11 12:50:20 +0300

    Define product list view and add filters

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Product
+from django.contrib.auth.models import Group
+
+admin.site.site_header = "PLANdesk Inventory Manager"
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'category')
+    list_filter = ('category', 'name')
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+#admin.site.unregister(Group)
