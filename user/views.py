a78e54a5041f0e41becec219e32c51a26579451e
frank-Mwangi <kungofrank@gmail.com>
2023-10-15 12:13:50 +0300

    Add profile Information dynamically

--- user/views.py
+++ user/views.py
@@ -18,3 +18,7 @@ def register(request):
         'form':form
     }
     return render(request, 'user/register.html', context)
+
+def profile(request):
+    """user profile page"""
+    return render(request, 'user/profile.html')
