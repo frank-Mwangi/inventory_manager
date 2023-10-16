6455d75a179da18068079cb3659679a054a98f9b
frank-Mwangi <kungofrank@gmail.com>
2023-10-16 12:59:25 +0300

    Implement logic for dashboard add product feature

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 @login_required
@@ -17,8 +18,17 @@ def product(request):
     items = Product.objects.all()
     # alternatively, use SQL queries like so:
     # items = Product.objects.raw('SELECT * FROM dashboard_product')
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('dashboard-product')
+
+    else:
+        form = ProductForm()
     context = {
         'items': items,
+        'form': form,
     }
     return render(request, 'dashboard/product.html', context)
 
