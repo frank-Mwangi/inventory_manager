c7473f1c9a9b3d20113c2baf27df118f3dc9c405
frank-Mwangi <kungofrank@gmail.com>
2023-10-16 12:24:41 +0300

    Implement profile update form

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 from django.contrib.auth.forms import UserCreationForm
 
 class CreateUserForm(UserCreationForm):
@@ -9,3 +10,13 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['address', 'phone', 'image']
